f605f3e969725d0c15c7e9dd8e19bc57107d0c94
boubacar906 <57671050+boubacar906@users.noreply.github.com>
2022-03-19 21:26:09 +0100

    Update Code.js

--- Code.js
+++ Code.js
@@ -1,1 +1,16 @@
 Var unirest = require("unirest")
+
+
+var requete1 = unirest("GET","http://localhost:3000/db?limit=10");
+requete1.end(function (res) {
+  if (res.error) throw new Error(res,error)
+  console.log(res.body)
+})
+
+
+
+var requete2 = unirest("GET","http://localhost:3000/db");
+requete2.end(function (res) {
+  if (res.error) throw new Error(res,error)
+  console.log(res.body.filter((db) => db.name.startsWith("M")))
+})
